58db209bf9a9c1b21154be8e871804bdb0c2d1f1
Dominik Skapin <dominik.skapin@gmail.com>
2020-10-17 00:54:40 +0200

    simple first ui

--- sketch.js
+++ sketch.js
@@ -1,34 +1,82 @@
 let red_clr;
 let blk_clr;
 let wht_clr;
-let n = 15;
+let rowLength;
 let boxWidth = 50;
 let board;
 let boxesRows;
 let boxesColumns;
 let prevXPos;
 let prevYPos;
+let mouseClickEnabled = true;
+let mouseDragEnabled = false;
+let startGameButtonDiv;
+let lengthInputDiv;
+let gameHeaderDiv;
+let widthPDiv;
+let saveImageButtonDiv;
+let mouseDraggedRadioDiv;
 
 function setup() {
+
+    startGameButtonDiv = document.getElementById("start_game_button");
+    gameStartDiv = document.getElementById("game_start");
+    canvasDiv = document.getElementById("canvas");
+    lengthInputDiv = document.getElementById("length_input");
+    gameHeaderDiv = document.getElementById("game_header");
+    widthPDiv = document.getElementById("width_p");
+    saveImageButtonDiv = document.getElementById("save_image");
+    mouseDraggedRadioDiv = document.getElementById("mouse_dragged_radio");
+
+    startGameButtonDiv.addEventListener("click", setupGame);
+    saveImageButtonDiv.addEventListener("click", saveImage);
+}
+
+function setupGame() {
+
+    gameHeaderDiv.style.display = "block";
+    gameStartDiv.style.display = "none";
+    canvasDiv.style.display = "block";
+    gameHeaderDiv.style.display = "block";
+
+    if (lengthInputDiv.value != null && lengthInputDiv.value > 3) {
+        rowLength = lengthInputDiv.value;
+    }
+    else if (lengthInputDiv.value > 30) {
+        rowLength = 30;
+    } else {
+        rowLength = 15;
+    }
+    widthPDiv.innerHTML = "Grid width: " + rowLength;
+
+    if (mouseDraggedRadioDiv.checked) {
+        mouseDragEnabled = true;
+        mouseClickEnabled = false;
+    } else {
+        mouseDragEnabled = false;
+        mouseClickEnabled = true;
+    }
+
+    let gameCanvas = createCanvas((rowLength + 3) * boxWidth, (rowLength + 3) * boxWidth);
+    gameCanvas.parent(canvasDiv);
+
     red_clr = color(122, 122, 122);
     blk_clr = color(0);
     wht_clr = color(255);
     background(255);
-    createCanvas((n + 3) * boxWidth, (n + 3) * boxWidth);
-
     board = [];
     setupNumberBoxes();
 
-    for (let x = 0; x < n; x++) {
+    for (let x = 0; x < rowLength; x++) {
         let column = [];
-        for (let y = 0; y < n; y++) {
+        for (let y = 0; y < rowLength; y++) {
             drawWhite(x, y);
             column.push(false);
-            if (y === n-1) {
-                drawNumberBoxColumn(x, n);
+            if (y === rowLength-1) {
+                drawNumberBoxColumn(x, rowLength);
             }
-            if (x === n-1) {
-                drawNumberBoxRow(n, y);
+            if (x === rowLength-1) {
+                drawNumberBoxRow(rowLength, y);
             }
         }
         board.push(column);
@@ -37,29 +85,34 @@ function setup() {
 }
 
 function mouseClicked() {
-    let xPos = Math.floor(mouseX / boxWidth);
-    let yPos = Math.floor(mouseY / boxWidth);
 
-    if (xPos >= 0 && xPos < n &&
-        yPos >= 0 && yPos < n) {
-        evaluateChange(xPos, yPos);
+    if (mouseClickEnabled) {
+        let xPos = Math.floor(mouseX / boxWidth);
+        let yPos = Math.floor(mouseY / boxWidth);
+
+        if (xPos >= 0 && xPos < rowLength &&
+            yPos >= 0 && yPos < rowLength) {
+            evaluateChange(xPos, yPos);
+        }
     }
 }
 
 function mouseDragged() {
-    let xPos = Math.floor(mouseX / boxWidth);
-    let yPos = Math.floor(mouseY / boxWidth);
 
-    if (prevXPos !== xPos || prevYPos !== yPos) {
+    if (mouseDragEnabled) {
+        let xPos = Math.floor(mouseX / boxWidth);
+        let yPos = Math.floor(mouseY / boxWidth);
 
-        if (xPos >= 0 && xPos < n &&
-            yPos >= 0 && yPos < n) {
+        if (prevXPos !== xPos || prevYPos !== yPos) {
+
+        if (xPos >= 0 && xPos < rowLength &&
+            yPos >= 0 && yPos < rowLength) {
             evaluateChange(xPos, yPos);
         }
     }
-
     prevXPos = xPos;
     prevYPos = yPos;
+    }
 }
 
 function evaluateChange(xPos, yPos) {
@@ -78,24 +131,16 @@ function evaluateChange(xPos, yPos) {
     }
 }
 
-// function updateBoxes(x, y, num) {
-//     boxesColumns[x] += num;
-//     boxesRows[y] += num;
-//     drawNumberBoxColumn(x, n);
-//     drawNumberBoxRow(n, y);
-//
-// }
-
 function updateBoxes(x, y) {
-    drawNumberBoxColumn(x, n);
-    drawNumberBoxRow(n, y);
+    drawNumberBoxColumn(x, rowLength);
+    drawNumberBoxRow(rowLength, y);
 
 }
 
 function evaluateRow(index) {
     let numbers = [];
     let sequence = 0;
-    for (let i = 0; i < n; i++) {
+    for (let i = 0; i < rowLength; i++) {
         if (board[i][index]) {
             sequence += 1;
         } else {
@@ -104,18 +149,17 @@ function evaluateRow(index) {
             }
             sequence = 0;
         }
-        if (i === n-1 && sequence > 0) {
+        if (i === rowLength-1 && sequence > 0) {
             numbers.push(sequence);
         }
     }
     boxesRows[index] = numbers;
-    // console.log("row: " + index + " ; " + boxesRows[index]);
 }
 
 function evaluateColumn(index) {
     let numbers = [];
     let sequence = 0;
-    for (let i = 0; i < n; i++) {
+    for (let i = 0; i < rowLength; i++) {
         if (board[index][i]) {
             sequence += 1;
         } else {
@@ -124,12 +168,11 @@ function evaluateColumn(index) {
             }
             sequence = 0;
         }
-        if (i === n-1 && sequence > 0) {
+        if (i === rowLength-1 && sequence > 0) {
             numbers.push(sequence);
         }
     }
     boxesColumns[index] = numbers;
-    // console.log("column: " + index + " ; " + boxesColumns[index]);
 }
 
 function drawWhite(x, y) {
@@ -189,9 +232,7 @@ function parseTextforBox(isHorizontal, index) {
             stringResult += "" + boxesColumns[index][i];
         }
     }
-
     return stringResult;
-
 }
 
 function setupNumberBoxes() {
@@ -199,8 +240,11 @@ function setupNumberBoxes() {
     boxesColumns = [];
     boxesRows = [];
 
-    for (let i = 0; i < n; i++) {
+    for (let i = 0; i < rowLength; i++) {
         boxesColumns.push([]);
         boxesRows.push([]);
     }
+}
+
+function saveImage() {
 }
